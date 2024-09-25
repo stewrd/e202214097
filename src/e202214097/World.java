24eb4b1200be836f5e0c0e1909e50fa298a22329
jujppy <jujppytter@gmail.com>
2024-09-25 09:57:43 +0900

    fifth

--- src/e202214097/World.java
+++ src/e202214097/World.java
@@ -3,6 +3,10 @@ package e202214097;
 public class World {
 
 	public static void run() {
-		System.out.println(202214097);
+		System.out.println("202214097");
+	}
+	
+	public static void run2() {
+		System.out.println("hello world");
 	}
 }
